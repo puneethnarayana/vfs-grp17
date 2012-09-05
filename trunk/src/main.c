b52688514b80689db642b5671179ce4d12137e55
rahul.vit09@gmail.com <rahul.vit09@gmail.com@db00c2b0-1461-884e-a205-dd71c8968857>
2012-09-05 10:17:04 +0000

    vfs large binary creation initial phase

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -13,24 +13,26 @@ void createVFS(char *name, long size) {
 	struct Block b1;
 	struct File_Descriptor fd1;
 	printf("--------------------Creating VFS-----------------------------");
-	b1.lfixed_size_buffer = 10240; // size of each file block = 10KB
-	m1.sfile_system_label = name;  //storing the VFS label
 	
+	m1.sfile_system_label = name;  //storing the VFS label
 	printf("\nvfs name : %s", m1.sfile_system_label);
 	printf("\nsize of meta header: %d", sizeof(m1));
 	m1.lsize = size - sizeof(m1); // size remaining after creation of meta header 
 	
 	printf("\nsize left after ceation of meta header: %ld", m1.lsize);
-	m1.ltotal_file_descriptors = (m1.lsize)/(sizeof(fd1) + b1.lfixed_size_buffer); // calculating the no. of file descriptors can be used in remaining space
+	m1.ltotal_file_descriptors = (m1.lsize)/(sizeof(fd1) + sizeof(b1)); // calculating the no. of file descriptors can be used in remaining space
+	//m1.ltotal_file_descriptors = 100; // calculating the no. of file descriptors can be used in remaining space
 	printf("\nsize of each file descriptor : %d", sizeof(fd1));
 	printf("\ntotal file descriptors : %ld", m1.ltotal_file_descriptors);
     m1.lsize = m1.lsize - (m1.ltotal_file_descriptors * sizeof(fd1)); // size remaining after creation of file descriptors
     
     printf("\nsize left after creation of file descriptors: %ld", m1.lsize);
-    b1.ltotal_file_blocks = (m1.lsize)/(b1.lfixed_size_buffer); // calculating the no. of file blocks can be used in remaining space
-    printf("\nsize of each file block : %ld", b1.lfixed_size_buffer);
-    printf("\ntotal file blocks : %ld", b1.ltotal_file_blocks);
-    m1.lsize = m1.lsize - (b1.ltotal_file_blocks * b1.lfixed_size_buffer); // calculating the space unused 
+    m1.ltotal_file_blocks = (m1.lsize)/(sizeof(b1)); // calculating the no. of file blocks can be used in remaining space
+    //b1.ltotal_file_blocks = 100; // calculating the no. of file blocks can be used in remaining space
+  
+    printf("\nsize of each file block : %d", sizeof(b1));
+    printf("\ntotal file blocks : %ld", m1.ltotal_file_blocks);
+    m1.lsize = m1.lsize - (m1.ltotal_file_blocks * sizeof(b1)); // calculating the space unused 
 	
 	printf("\nsize left after creation of file block: %ld\n", m1.lsize);
     
@@ -51,12 +53,12 @@ void createVFS(char *name, long size) {
 		for (; i<= m1.ltotal_file_descriptors; i++) {
 			fwrite(&fd1, sizeof(fd1), 1, fptr); // writing file descriptors
 		}
-		printf("file  descriptor created\n");
+		printf("file  descriptor created\n"); 
 	
-		for (; j<= b1.ltotal_file_blocks; j++) {
-			fwrite(&b1, b1.lfixed_size_buffer, 1, fptr); // writing file blocks
+	   for (; j<= m1.ltotal_file_blocks; j++) {
+			fwrite(&b1, sizeof(b1), 1, fptr); // writing file blocks
 		}
-		printf("file block created\n");
+		printf("file block created\n"); 
 		fclose(fptr); // closing the file to store it in persistent memory
 	}
 }
